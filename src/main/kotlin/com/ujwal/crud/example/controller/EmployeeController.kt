76140971116e632c0ea980e3799059c00cb92e3a
Ujwal Thote <thoteujwal97@gmail.com>
2022-10-25 22:26:37 +0530

    Add search api

--- src/main/kotlin/com/ujwal/crud/example/controller/EmployeeController.kt
+++ src/main/kotlin/com/ujwal/crud/example/controller/EmployeeController.kt
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -66,4 +67,8 @@ class EmployeeController {
             throw ResourceNotFoundException()
         }
     }
+
+    @GetMapping("search")
+    fun search(@RequestParam("firstName") firstName: String): ResponseEntity<List<Employee>> =
+        ResponseEntity.ok(employeeRepository.findByFirstName(firstName))
 }
--- src/main/kotlin/com/ujwal/crud/example/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/ujwal/crud/example/repository/EmployeeRepository.kt
@@ -1,8 +1,15 @@
 package com.ujwal.crud.example.repository
 
 import com.ujwal.crud.example.model.Employee
+import com.ujwal.crud.example.model.TABLE_NAME
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface EmployeeRepository : JpaRepository<Employee, Long>
+interface EmployeeRepository : JpaRepository<Employee, Long> {
+
+    @Query
+    fun findByFirstName(firstName: String): List<Employee>
+
+}
