ce9a2e469bdf640ebde611ec915586e57e1b43c8
Ujwal Thote <thoteujwal97@gmail.com>
2022-10-25 22:51:10 +0530

    Optimise imports

--- src/main/kotlin/com/ujwal/crud/example/controller/EmployeeController.kt
+++ src/main/kotlin/com/ujwal/crud/example/controller/EmployeeController.kt
@@ -5,15 +5,7 @@ import com.ujwal.crud.example.model.Employee
 import com.ujwal.crud.example.repository.EmployeeRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/employees")
--- src/main/kotlin/com/ujwal/crud/example/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/ujwal/crud/example/repository/EmployeeRepository.kt
@@ -1,7 +1,6 @@
 package com.ujwal.crud.example.repository
 
 import com.ujwal.crud.example.model.Employee
-import com.ujwal.crud.example.model.TABLE_NAME
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
